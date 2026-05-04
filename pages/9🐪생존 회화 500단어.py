e701cc9a67eb7b0ee1b97ab38530952ef84b4602
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-05-04 20:45:07 +0900

    Update 9🐪생존 회화 500단어.py

--- pages/9🐪생존 회화 500단어.py
+++ pages/9🐪생존 회화 500단어.py
@@ -258,9 +258,9 @@ st.markdown(
     <div class="hero-box">
         <div class="hero-title">🌟 오늘의 학습 방식</div>
         <div class="hero-text">
-            • 각 테마는 <b>오늘의 생존 대화</b>로 시작합니다.<br>
+            • 이 단어들만 외우면 미국에서 생존이 가능합니다. 힘내봅시다.<br>
             • 대화를 듣고, 바로 아래에서 핵심 단어를 익힙니다.<br>
-            • 이 단어들만 외우면 미국에서 생존이 가능합니다. 힘냅시다.<br>
+
     </div>
     """,
     unsafe_allow_html=True
