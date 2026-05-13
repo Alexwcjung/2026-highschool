1b978ef849b10229d348c4e7d8085c75f6c39412
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-05-13 13:13:59 +0900

    Add soccer conversation with Ronaldo

--- pages/93🌷 향긋한 글향.py
+++ pages/93🌷 향긋한 글향.py
@@ -1,1 +1,16 @@
 
+st.markdown("## ⚽ Soccer Talk with Ronaldo")
+
+st.image("pages/images/ronaldo.png", use_container_width=True)
+
+st.markdown("""
+### Reading Practice
+
+Ronaldo: Hi! Do you like soccer?
+
+Me: Yes, I do. I really like soccer.
+
+Ronaldo: That’s great. Who is your favorite player?
+
+Me: You are my favorite player, Ronaldo.
+""")
