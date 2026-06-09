00c1d8a0e566cd6cd9d8d623e2f491212e7cb7f2
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-06-09 13:15:03 +0900

    Update 동영상.py

--- pages/동영상.py
+++ pages/동영상.py
@@ -16,7 +16,7 @@ st.title("🎬 영상 보기")
 videos = {
     "영상 1": "videos/Streamlit_앱_활용_영어_수업 (1).mp4",
     "영상 2": "videos/스트레스_제로_영어_수업.mp4",
-    "영상 3": "videos/video3.mp4",
+    "영상 3": "videos/스트레스_제로_스마트폰_앱_아지트.mp4",
     "영상 4": "videos/video4.mp4",
 }
 
