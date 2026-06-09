ca62547f1b9a87d4aecf53fec890b8100ea6fc24
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-06-09 13:06:08 +0900

    Update 동영상.py

--- pages/동영상.py
+++ pages/동영상.py
@@ -1,1 +1,10 @@
 
+import streamlit as st
+
+st.set_page_config(page_title="Video Learning", page_icon="🎬", layout="wide")
+
+st.title("🎬 동영상 수업")
+
+video_path = "videos/Streamlit_앱_활용_영어_수업 (1).mp4"
+
+st.video(video_path)
