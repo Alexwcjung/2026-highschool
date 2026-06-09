1cc2c79e2010d449aa11ab7d383bdb12b91accfe
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-06-09 19:10:17 +0900

    Update 1🐻앱 소개.py

--- pages/1🐻앱 소개.py
+++ pages/1🐻앱 소개.py
@@ -345,7 +345,7 @@ with right:
 # =========================
 st.markdown('<div class="section-title">🎬 앱 소개 영상</div>', unsafe_allow_html=True) 
 
-video_path = "videos/스마트폰_영어_아지트.mp44"
+video_path = "videos/스마트폰_영어_아지트.mp4"
 
 st.markdown("<div class='video-wrap'>", unsafe_allow_html=True)
 st.markdown("<div class='video-title'>🎬 스트레스 제로 스마트폰 앱 아지트</div>", unsafe_allow_html=True)
